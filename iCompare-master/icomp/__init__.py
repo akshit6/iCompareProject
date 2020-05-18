e739a17c4d044877d92cb5802114ba2d97f16402
akshit6 <akshit.gs@somaiya.edu>
2020-05-18 14:02:29 +0530

    added session variables

---
+++ iCompare-master/icomp/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '96e582be6947f9b29b1cd0f615a33600'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+# login_manager = LoginManager(app)
+from icomp import routes
